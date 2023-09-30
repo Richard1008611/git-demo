d93144d6cadcffd28af067b6323287fe20a00a3c
xiaokong <643669382@qq.com>
2023-09-30 18:32:26 +0800

    first commit

---
+++ src/test01.java
@@ -0,0 +1,37 @@
+import java.util.function.DoubleToIntFunction;
+public class test01 {
+    public static void main(String[] args){
+        int a[][]=new int[5][5];
+        for(int i=0;i<=4;i++){
+            a[i][0]=1;
+            a[i][i]=1;
+        }
+        for(int i=0;i<=4;i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                a[i][j] = a[i - 1][j - 1] + a[i - 1][j - 1];
+
+            }
+        }
+            for(int i=0;i<=4;i++) {
+                for(int j=0;j<=i;j++){
+                    System.out.print(a[i][j]+" ");
+                }
+                System.out.println();
+            }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
